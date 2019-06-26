cc23ef6359554711614b514ef92c03d0fef66071
20181102931 <18647197975@163.com>
2019-06-26 21:32:35 +0800

    Initial Commit

---
+++ 学生成绩打分111/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  学生成绩打分111
+//
+//  Created by s20181102931 on 2019/6/26.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
